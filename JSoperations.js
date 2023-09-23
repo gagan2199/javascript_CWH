3593eb62e222d488d0e87b9a9c527fe64f189c7f
Gagan <gagank2199@gmail.com>
2023-09-23 18:53:34 +0530

    js string opn

---
+++ JSoperations.js
@@ -0,0 +1,15 @@
+let nam="gagan";
+
+console.log(nam);
+
+nam[0]="G";
+
+console.log(nam);
+
+nam=nam.toUpperCase();
+
+
+console.log(nam.replace("G","g"));
+console.log(nam.replaceAll("G","g"));
+
+console.log(nam);
