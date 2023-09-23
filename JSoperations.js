0817c151042e7543b264239aa7db6aba167b8c42
Gagan <gagank2199@gmail.com>
2023-09-23 22:43:38 +0530

    reverse added

--- JSoperations.js
+++ JSoperations.js
@@ -25,4 +25,22 @@ console.log(arr.sort((a,b)=>        // will print ascending order
     return a-b;
 }));
 
+//splice
+
+let spl=arr.splice(4,2,1022,1023);
+console.log(arr);         //will return new arr
+console.log(spl);        //spl will return deleted values
+
+let b=[1,2,3,4,5,6,7];
+
+let slc= b.slice(3,5);    // git value from index 3 to 4.  .....5 index will not add
+
+console.log(b);
+console.log(slc);
+
+let c=[10,20,30,40,50];
+
+let rev=c.reverse();       // rev is for array
+
+console.log(rev);
 
