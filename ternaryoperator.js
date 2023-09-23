527b30cb04f87a969dd5db1d64ee575de8388591
Gagan <gagank2199@gmail.com>
2023-09-23 16:44:07 +0530

    conditional and ternary operator

---
+++ ternaryoperator.js
@@ -0,0 +1,12 @@
+
+// let age;
+let age = document.getElementById('age');
+
+age.addEventListener('click', (element) => {
+    age = prompt("write here");
+
+    console.log((age >= 18) ? "eligible" : "not eligible");
+    console.warn((age >= 18) ? "eligible" : "not eligible");
+})
+
+
