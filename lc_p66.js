88d75a16a2e717b2df7daf4246546c87ae9c4846
Gagan <gagank2199@gmail.com>
2023-09-27 22:22:33 +0530

    lc p66

---
+++ lc_p66.js
@@ -0,0 +1,35 @@
+
+var plusOne = function(digits) {
+    var len =  digits.length;
+       var num;
+       var add = 1;
+
+       for(let i = len-1;i>=0;i--)
+       {
+           num = digits[i]+add;
+           if(num == 10)
+           {
+               digits[i] = 0;
+           }
+           else
+           {
+               digits[i] = num;
+               return digits;
+           }
+       }
+
+       digits.unshift(1);
+       return digits;
+};
+
+let digits=[];
+let n=prompt("enter the number of members");
+for(let i=0;i<n;i++)
+{
+    digits[i]=prompt("enter the value of member");
+}
+
+
+console.log(digits);
+
+console.log(plusOne);
