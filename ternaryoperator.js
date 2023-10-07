cfbcce2786bfb1e04951d3bbff3428a5f4e54198
Gagan <gagank2199@gmail.com>
2023-10-07 17:54:14 +0530

    to

--- ternaryoperator.js
+++ ternaryoperator.js
@@ -3,7 +3,7 @@
 let age = document.getElementById('age');
 
 age.addEventListener('click', (element) => {
-    age = prompt("write here");
+    age = prompt("write age here");
 
     console.log((age >= 18) ? "eligible" : "not eligible");
     console.warn((age >= 18) ? "eligible" : "not eligible");
