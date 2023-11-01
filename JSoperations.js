508833593a5176d28369717d304b647180e152bf
Gagan Kumar <114229570+gagan2199@users.noreply.github.com>
2023-11-01 11:18:57 +0530

    Update JSoperations.js

--- JSoperations.js
+++ JSoperations.js
@@ -18,7 +18,7 @@ console.log(nam);
 
 let arr=[2,24,54,24,75,446,353,98,342];
 
-console.log(arr.sort());  // will print 1,2,3,4,5,6,7,8,9 in first item
+console.log(arr.sort());  // will print 1,2,3,4,5,6,7,8,9 in first item of array
 
 console.log(arr.sort((a,b)=>        // will print ascending order
 {
